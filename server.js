16eae54702ac9ecd809ee5647e82d5d4f9209901
S Suwannapaond <saharat.rus@gmail.com>
2025-03-20 13:13:58 +0700

    update

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const app = express();
 const path = require("path");
 const routes = require("./routes/index");
 
-// ตั้งค่า EJS เป็น View Engine
+//* ตั้งค่า EJS เป็น View Engine
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-// ใช้ Routes ที่แยกไว้
+//* ใช้ Routes ที่แยกไว้
 app.use("/", routes);
 
-// เปิดเซิร์ฟเวอร์ที่ port 3000
+//* เปิดเซิร์ฟเวอร์ที่ port 3000
 app.listen(3000, () => {
   console.log("Server is running on http://localhost:3000");
 });
